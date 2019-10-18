8bce44cdd1627967adaf2d68d06a2774995c429d
Ivan R. Judson <irjudson@gmail.com>
2019-10-18 15:35:55 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # synthea-api
+
+This project presents a REST API to use synthea as a service.
+
+to build, ensure you have:
+
+# jdk 8
+# maven
+# an editor (of your choice)
+
+then, to build, run:
+
+```mvn clean compile assembly:single```
+
+to run the service locally, run:
+
+```java -jar target/SyntheaAPI-1.0-SNAPSHOT-jar-with-dependencies.jar org.adghealth.SyntheaAPI```
---
+++ src/main/java/org/adghealth/SyntheaAPI.java
@@ -0,0 +1,85 @@
+package org.adghealth;
+
+import static spark.Spark.delete;
+import static spark.Spark.get;
+import static spark.Spark.options;
+import static spark.Spark.post;
+import static spark.Spark.put;
+
+import com.google.gson.Gson;
+
+import org.mitre.synthea.engine.Generator;
+import org.mitre.synthea.helpers.Config;
+
+public class SyntheaAPI {
+    public static void main(String[] args) {
+        // final UserService userService = new UserServiceMapImpl();
+        get("/hello", (request, response) -> "Hello, Synthea");
+
+        get("/hello/:name", (request, response) -> {
+            return "Hello: " + request.params(":name");
+        });
+
+        get("/data", (request, response) -> {
+            Generator.GeneratorOptions options = new Generator.GeneratorOptions();
+            Generator generator = new Generator(options);
+            generator.run();
+            return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS));
+        });
+
+        post("/data", (request, response) -> {
+            response.type("application/json");
+            Generator.GeneratorOptions options = new Gson().fromJson(request.body(), Generator.GeneratorOptions.class);
+            Generator generator = new Generator(options);
+            generator.run();
+            return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS));
+        });
+
+        // post("/users", (request, response) -> {
+        //     response.type("application/json");
+
+        //     User user = new Gson().fromJson(request.body(), User.class);
+        //     userService.addUser(user);
+
+        //     return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS));
+        // });
+
+        // get("/users", (request, response) -> {
+        //     response.type("application/json");
+
+        //     return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(userService.getUsers())));
+        // });
+
+        // get("/users/:id", (request, response) -> {
+        //     response.type("application/json");
+
+        //     return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(userService.getUser(request.params(":id")))));
+        // });
+
+        // put("/users/:id", (request, response) -> {
+        //     response.type("application/json");
+
+        //     User toEdit = new Gson().fromJson(request.body(), User.class);
+        //     User editedUser = userService.editUser(toEdit);
+
+        //     if (editedUser != null) {
+        //         return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(editedUser)));
+        //     } else {
+        //         return new Gson().toJson(new StandardResponse(StatusResponse.ERROR, new Gson().toJson("User not found or error in edit")));
+        //     }
+        // });
+
+        // delete("/users/:id", (request, response) -> {
+        //     response.type("application/json");
+
+        //     userService.deleteUser(request.params(":id"));
+        //     return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, "user deleted"));
+        // });
+
+        // options("/users/:id", (request, response) -> {
+        //     response.type("application/json");
+
+        //     return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, (userService.userExist(request.params(":id"))) ? "User exists" : "User does not exists"));
+        // });
+    }
+}
